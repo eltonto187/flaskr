56aba676e3bbc02a56b634cb72aea4a2378cd4bb
eltonto <eltonto187@gmail.com>
2016-02-24 23:55:31 +0800

    添加表单,Flask-wtf扩展

--- hello.py
+++ hello.py
@@ -3,11 +3,21 @@ from flask import Flask, render_template
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required, Email
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+class NameForm(Form):
+    name = StringField('你的名字？', validators=[Required()])
+    email = StringField('你的电子邮箱', validators=[Email()])
+    submit = SubmitField('提交')
 
 
 @app.errorhandler(404)
@@ -20,9 +30,18 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    email = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        form.name.data = ''
+        form.email.data = ''
+    return render_template('index.html', name=name, email=email, form=form,
+                           current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
