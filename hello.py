3671a194d75083b76b86358a1b0300e4a9319841
eltonto <eltonto187@gmail.com>
2016-02-26 10:29:10 +0800

    添加数据库，使用Flask-SQLAlchemy扩展

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 from flask import Flask, render_template, redirect, session, url_for, flash
 from flask.ext.script import Manager
@@ -6,13 +7,39 @@ from flask.ext.moment import Moment
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required, Email
+from flask.ext.sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 class NameForm(Form):
     name = StringField('你的名字？', validators=[Required()])
@@ -35,13 +62,22 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['konwn'] = False
+        else:
+            session['konwn'] = True
         if old_name is not None and old_name != form.name.data:
             flash('看起来你已经改变了你输入的名字')
         session['name'] = form.name.data
         session['email'] = form.email.data
         return redirect(url_for('index'))
     return render_template('index.html', name=session.get('name'),
-        email=session.get('email'), form=form, current_time=datetime.utcnow())
+        email=session.get('email'), form=form,
+        known=session.get('konwn', False),
+        current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
